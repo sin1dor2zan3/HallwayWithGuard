16ac49b8af4a41a8a5affb988feffc0ea004b3bb
SlaneTrosk <jacobekuhlman@yahoo.com>
2026-03-03 23:43:39 -0500

    ui/ux

---
+++ Assets/WinMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class WinMenu : MonoBehaviour {
+
+    public void ReplayButton() 
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitButton() 
+    {
+        Application.Quit();
+    }
+
+}
