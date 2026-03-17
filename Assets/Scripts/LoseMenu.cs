86790c682f2400b9bed489c27b4eacdc09b3b77b
sin1dor2zan3 <sin1dor2zan3@gmail.com>
2026-03-17 00:42:43 -0400

    Fixed Lose Menu
    ~ Edited Lose Menu Script

--- Assets/Scripts/LoseMenu.cs
+++ Assets/Scripts/LoseMenu.cs
@@ -1,23 +1,38 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
-public class LoseMenu : MonoBehaviour {
+public class LoseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject firstButton;
 
-    public void ReplayButton() 
+    void Start()
     {
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        if (EventSystem.current != null && firstButton != null)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(firstButton);
+        }
+    }
+
+    public void ReplayButton()
+    {
+        Time.timeScale = 1f;
         SceneManager.LoadSceneAsync(1);
     }
 
-    public void QuitButton() 
+    public void QuitButton()
     {
         Application.Quit();
     }
 
     public void MainMenuButton()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadSceneAsync(0);
     }
-
 }
